ce5fa3a5571497c253acce5f0cf3ef9d63ff69da
chathuranga1931 <chathurangadissanayaka@yahoo.com>
2015-12-03 19:10:04 +0530

    first commit

---
+++ BackItUp/BackItUp/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace BackItUp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(776, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(667, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(103, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(667, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(776, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Source URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Destination URL";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(776, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Backup";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(103, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Back up folder";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 98);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(839, 439);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(695, 69);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Load";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(695, 69);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Try again";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(12, 560);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(837, 27);
+            this.progressBar2.Step = 1000;
+            this.progressBar2.TabIndex = 1;
+            this.progressBar2.Click += new System.EventHandler(this.progressBar2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(241, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Total Size";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(301, 66);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(106, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(413, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "KB";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 604);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Back It up";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ BackItUp/BackItUp/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BackItUp
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog openFileDialog1 = new FolderBrowserDialog();
+        static List<FileInfo> sfiles = new List<FileInfo>();  // List that will hold the files and subfiles in path
+        static List<DirectoryInfo> sfolders = new List<DirectoryInfo>(); // List that hold direcotries that cannot be accessed
+
+        static List<FileInfo> dfiles = new List<FileInfo>();  // List that will hold the files and subfiles in path
+        static List<DirectoryInfo> dfolders = new List<DirectoryInfo>(); // List that hold direcotries that cannot be accessed
+
+        static List<FileInfo> newFiles = new List<FileInfo>();
+
+        private string SourceAdd;
+        private string DestinationAdd;
+        private string BackUpFolder;
+
+        private string Source, Backup;
+        private long sizeOfSource;
+        private long readFileSize=0;
+
+        private Thread threadBackup;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            //string[] files = Directory.GetFiles(openFileDialog1.SelectedPath);
+            //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+
+            if (result.ToString() == "OK")
+            {
+                textBox1.Text = openFileDialog1.SelectedPath.ToString();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            //string[] files = Directory.GetFiles(openFileDialog1.SelectedPath);
+            //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+
+
+            if (result.ToString() == "OK" )
+            {
+                textBox2.Text = openFileDialog1.SelectedPath.ToString();
+                
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*
+            DirectoryInfo sdi = new DirectoryInfo(SourceAdd);
+            sFullDirList(sdi, "*");
+            Console.WriteLine("Done");
+            Console.Read();
+             */
+            richTextBox1.Clear();
+            threadBackup = new Thread(this.BackingUp);
+            threadBackup.Start();
+
+            button3.Enabled = false;
+      
+        }
+
+        static void sFullDirList(DirectoryInfo dir, string searchPattern)
+        {
+            // Console.WriteLine("Directory {0}", dir.FullName);
+            // list the files
+            try
+            {
+                foreach (FileInfo f in dir.GetFiles(searchPattern))
+                {
+                    Console.WriteLine("File {0}", f.FullName);
+                    sfiles.Add(f);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Directory {0}  \n could not be accessed!!!!", dir.FullName);
+                return;  // We alredy got an error trying to access dir so dont try to access it again
+            }
+
+            // process each directory
+            // If I have been able to see the files in the directory I should also be able 
+            // to look at its directories so I dont think I should place this in a try catch block
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                sfolders.Add(d);
+                sFullDirList(d, searchPattern);
+            }
+        }
+        static void dFullDirList(DirectoryInfo dir, string searchPattern)
+        {
+            // Console.WriteLine("Directory {0}", dir.FullName);
+            // list the files
+            try
+            {
+                foreach (FileInfo f in dir.GetFiles(searchPattern))
+                {
+                    Console.WriteLine("File {0}", f.FullName);
+                    dfiles.Add(f);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Directory {0}  \n could not be accessed!!!!", dir.FullName);
+                return;  // We alredy got an error trying to access dir so dont try to access it again
+            }
+
+            // process each directory
+            // If I have been able to see the files in the directory I should also be able 
+            // to look at its directories so I dont think I should place this in a try catch block
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                dfolders.Add(d);
+                dFullDirList(d, searchPattern);
+            }
+        }
+        private void createBackUpFolder(string destUrl, string sorceUrl)
+        {
+            string[] a = sorceUrl.Split('\\');
+            string backUpfolder = a[a.Count() - 1];
+
+            if (checkFolder(destUrl, backUpfolder))
+            {
+                this.Invoke((MethodInvoker)delegate()
+                {
+                    richTextBox1.AppendText("Folder exist " + '\n');
+                });
+            }
+            else
+            {
+                this.Invoke((MethodInvoker)delegate()
+                {
+                    richTextBox1.AppendText("Folder does not exist" + '\n');
+                });
+                try
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(destUrl + '\\' + backUpfolder);
+                    this.Invoke((MethodInvoker)delegate()
+                    {
+                        richTextBox1.AppendText(" Create directory successfully" + '\n');
+                    });
+                }
+                catch (Exception e)
+                {
+                    this.Invoke((MethodInvoker)delegate()
+                    {
+                        richTextBox1.AppendText("Folder does not create successfully " + '\n');
+                    });
+                }
+                
+
+            }
+        }
+        private bool checkFolder(string url, string folderName)
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(url); // Populates field with all Sub Folders
+            DirectoryInfo[] directorys = dinfo.GetDirectories();
+            foreach (DirectoryInfo directory in directorys)
+            {
+                this.Invoke((MethodInvoker)delegate()
+                {
+                    richTextBox1.AppendText(directory.Name + '\n');
+                }); 
+                if (folderName == directory.Name) return true;
+            }
+            return false;
+        }
+
+        private void BackUpFiles(DirectoryInfo SourceAddRef, DirectoryInfo DestinationAddRef)
+        {
+            foreach (FileInfo f in SourceAddRef.GetFiles("*"))
+            {
+                //Console.WriteLine("File {0}", f.Name);
+                //sfiles.Add(f);
+                readFileSize = readFileSize + f.Length;
+                this.Invoke((MethodInvoker)delegate()
+                {
+                    progressBar2.Value = (int)(readFileSize * 100 / sizeOfSource);
+                });
+
+                if(progressBar2.Value==100)
+                {
+                    this.Invoke((MethodInvoker)delegate()
+                    {
+                        button3.Enabled = true;
+                    }); 
+                }
+
+                int count = 0;
+                foreach (FileInfo j in DestinationAddRef.GetFiles("*"))
+                {
+                    count++;
+                }
+                foreach (FileInfo j in DestinationAddRef.GetFiles("*"))
+                {
+                    //Console.WriteLine("File {0}", f.Name);
+                    //sfiles.Add(f);
+                    if (f.Name == j.Name)
+                    {
+                        if (f.LastWriteTime != j.LastWriteTime)
+                        {
+                            File.Copy(SourceAddRef.FullName + "\\" + f.Name, DestinationAddRef.FullName + "\\" + f.Name, true);
+                            this.Invoke((MethodInvoker)delegate()
+                            {
+                                richTextBox1.AppendText("Overwrite file : " + DestinationAddRef.FullName + "\\" + f.Name + '\n');
+                            });
+                            
+                        }
+                        break;
+                    }
+                    count--;
+                }
+                if (count == 0)
+                {
+                    //file does not in the folder so copy the file;
+                    File.Copy(SourceAddRef.FullName + "\\" + f.Name, DestinationAddRef.FullName + "\\" + f.Name);
+                    this.Invoke((MethodInvoker)delegate()
+                    {
+                        richTextBox1.AppendText("Coppied file : " + DestinationAddRef.FullName + "\\" + f.Name + '\n');
+                    });
+                }
+            } 
+        }
+        private void BackUpFolders(DirectoryInfo SourceAddRef, DirectoryInfo DestinationAddRef)
+        {
+            foreach (DirectoryInfo f in SourceAddRef.GetDirectories())
+            {
+                //Console.WriteLine("File {0}", f.Name);
+                //sfiles.Add(f);
+                int count = 0;
+                foreach (DirectoryInfo j in DestinationAddRef.GetDirectories())
+                {
+                    count++;
+                }
+                foreach (DirectoryInfo j in DestinationAddRef.GetDirectories())
+                {
+                    //Console.WriteLine("File {0}", f.Name);
+                    //sfiles.Add(f);
+                    if (f.Name == j.Name)
+                    {
+                        BackUpFolders(new DirectoryInfo(SourceAddRef.FullName + "\\" + f.Name), new DirectoryInfo(DestinationAddRef.FullName + "\\" + f.Name));
+                        break;
+                    }
+                    count--;
+                }
+                if (count == 0)
+                {
+                    //folder does not in the folder so create a folder;
+                    Directory.CreateDirectory(DestinationAddRef.FullName + "\\" + f.Name);
+                    this.Invoke((MethodInvoker)delegate()
+                    {
+                        richTextBox1.AppendText("Directory Created : " + DestinationAddRef.FullName + "\\" + f.Name + '\n');
+                    });
+                    BackUpFolders(new DirectoryInfo(SourceAddRef.FullName + "\\" + f.Name), new DirectoryInfo(DestinationAddRef.FullName + "\\" + f.Name));
+                }
+            }            
+            BackUpFiles( SourceAddRef, DestinationAddRef); 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string[] a = textBox1.Text.Split('\\');
+            textBox3.Text = BackUpFolder = a[a.Count() - 1];
+            SourceAdd = textBox1.Text;
+            Source = SourceAdd;
+
+            DestinationAdd = textBox2.Text;
+            string tmp = DestinationAdd + "\\" + BackUpFolder;
+            Backup = tmp;
+
+            try
+            {
+                DirectoryInfo h = new DirectoryInfo(textBox1.Text);
+                DirectoryInfo l = new DirectoryInfo(textBox2.Text);
+
+                sizeOfSource = GetDirectorySize(textBox1.Text);
+                //MessageBox.Show(dirSize.ToString());
+
+                button3.Enabled = true;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button5.Visible = true;
+                button4.Visible = false;
+
+                textBox4.Text = (sizeOfSource/1024).ToString();
+            }
+            catch( Exception eb) 
+            {
+                MessageBox.Show("Your Source or Destination is not valid");
+                button3.Enabled = false;
+            }
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button5.Visible = false;
+            button4.Visible = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            button3.Enabled = false;
+        }
+        private static long GetDirectorySize(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+            string[] subdirectories = Directory.GetDirectories(path);
+
+            long size = files.Sum(x => new FileInfo(x).Length);
+            foreach (string s in subdirectories)
+                size += GetDirectorySize(s);
+
+            return size;
+        }
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BackingUp()
+        {
+            readFileSize = 0;
+            this.Invoke((MethodInvoker)delegate()
+            {
+                progressBar2.Value = 0;
+            });
+            createBackUpFolder(DestinationAdd, SourceAdd);
+            BackUpFolders(new DirectoryInfo(Source), new DirectoryInfo(Backup));
+            this.Invoke((MethodInvoker)delegate()
+            {
+                MessageBox.Show("Backup complete");
+            });
+            this.threadBackup.Abort();
+        }
+    }
+}
+
+/*
+ static void Main(string[] args)
+{
+    DirectoryInfo di = new DirectoryInfo("A:\\");
+    FullDirList(di, "*");
+    Console.WriteLine("Done");
+    Console.Read();
+}
+
+
+
+}
+ */
